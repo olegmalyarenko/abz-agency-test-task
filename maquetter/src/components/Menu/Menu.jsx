4550a78cc52aafd0668805d6f6368d56843e7f32
olegmalyarenko <oi.malyarenko@gmail.com>
2020-09-01 00:39:50 +0300

    feat: update Menu comp

--- maquetter/src/components/Menu/Menu.jsx
+++ maquetter/src/components/Menu/Menu.jsx
@@ -1,15 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Logo from '../Logo';
+import { ReactComponent as MobileIcon } from './menu icon.svg';
 import './Menu.scss';
-const Menu = () => {
+export default class Menu extends Component {
+    state = {
+        width: 0
+    }
+    
+    setSize = () =>  {
+        this.setState({
+            width: window.innerWidth
+        });
+      }
+
+    componentDidMount() {
+        this.setSize();
+        window.addEventListener('resize', this.setSize);
+      }
+    
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.setSize);
+      }
+    
+
+    render() {
+            
+
+        
+        if(this.state.width < 800) {
+            return (
+                <div>
+                    <div className="conteiner">
+                     <Logo/>
+                     <MobileIcon />
+                    </div>
+        
+                </div>
+            )
+
+        }
+    
     return (
         <div>
             <div className="conteiner">
              <Logo/>
+             <ul>
+                <li>About me</li>
+                <li>Relationships</li>
+                <li>Requirements</li>
+                <li>Users</li>
+                <li>Sign Up</li>
+    
+              </ul>
             </div>
 
         </div>
     )
+    }
 
 }
-export default Menu;
