3d3546fba6f2438e0c18439fd4daa4c346efba21
olegmalyarenko <oi.malyarenko@gmail.com>
2020-09-12 17:20:06 +0300

    feat: add mobile menu styles

--- maquetter/src/components/SideMenu/SideMenu.jsx
+++ maquetter/src/components/SideMenu/SideMenu.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
 import './SideMenu.scss';
+import Logo from '../Logo';
 
 const SideMenu = (props) => {
   const open = "side-menu open";
   const close = "side-menu"
   const visibleMenu = props.visible ? open : close;
     return ( 
-      <nav className={visibleMenu} >
-          <ul>
-           <li><a href="/">Привет</a></li>
-           <li><a href="/">Олег</a></li>
-          </ul>
-      </nav>
+      <div className={visibleMenu} >
+        <Logo />
+        <ul className='side-menu__nav'>
+           <li><a href="/" className='side-menu__item'>About me</a></li>
+           <li><a href="/" className='side-menu__item'>Relationships</a></li>
+           <li><a href="/" className='side-menu__item'>Users</a></li>
+           <li><a href="/" className='side-menu__item'>Sign Up </a></li>
+           <li><a href="/" className='side-menu__item'>Terms and Conditions</a></li>
+        </ul>
+
+        <ul className='side-menu__nav'>
+           <li><a href="/" className='side-menu__item'>How it works</a></li>
+           <li><a href="/" className='side-menu__item'>Partnership</a></li>
+           <li><a href="/" className='side-menu__item'>Help</a></li>
+           <li><a href="/" className='side-menu__item'>Live testimonial</a></li>
+           <li><a href="/" className='side-menu__item'>Contact us</a></li>
+        </ul>
+
+        <ul className='side-menu__nav'>
+           <li><a href="/" className='side-menu__item'>Articles</a></li>
+           <li><a href="/" className='side-menu__item'>Our news</a></li>
+           <li><a href="/" className='side-menu__item'>Testimonials</a></li>
+           <li><a href="/" className='side-menu__item'>Licenses</a></li>
+           <li><a href="/" className='side-menu__item'>Privacy Policy</a></li>
+        </ul>
+
+      </div>
         
     )
 };
